4d79dfe8a79a1aee26ca98d9f71d5713768fe125
Parker Lacey <parkerlacey2424@gmail.com>
2022-09-01 15:33:59 -0600

    adding initial files

---
+++ script.js
@@ -0,0 +1,44 @@
+
+var today = moment();
+
+
+$(document).ready(function(){
+    $("#currentDay").text(moment().format("dddd, MMMM Do")); 
+    $(".saveBtn").on("click", function(){
+        var plannerText = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, plannerText);
+
+    }); 
+    
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    // add more rows for more time
+    function timeChange(){
+        var currentHour = moment().hour(); // Number
+        
+        $('.time-block').each(function(){
+            var idTime = parseInt($(this).attr("id"));
+            if (idTime < currentHour){
+                $(this).addClass("past");
+            } else if (idTime === currentHour){
+                $(this).addClass("present");
+                $(this).removeClass("past");
+            } else {$(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            }
+        })
+    }
+    timeChange();
+    var timeInterval = setInterval(timeChange, 1000);
+
+
+});
